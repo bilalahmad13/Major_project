b3aaba9dd2855bef4fc5c65250d531832a4f1275
BILAL AHMAD <officialbilalahmad123@gmail.com>
2025-12-04 11:45:38 +0530

    add all files

---
+++ include/logger.h
@@ -0,0 +1,13 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void addMood();
+void viewLogs();
+void searchByDate();
+
+#endif
